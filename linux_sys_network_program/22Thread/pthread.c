86a23414c4a21fd4546455aae7f948701f814bd6
1617me <ourxrr@gmail.com>
2016-09-26 15:03:52 +0800

    new pthread.c test commit && push

---
+++ linux_sys_network_program/22Thread/pthread.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *th_func(void *arg){
+    int *p = (int *)arg;
+    printf("thread pid: %d\n", getpid());
+    // 1.....
+    printf("thread id: %x\n", (unsigned int)pthread_self());
+    printf("thread *arg:%d\n", *p);
+    //sleep(1);
+}
+
+int main(int argc, char **argv){
+    pthread_t tid;
+    int n = 10;
+    // 这里获取到的tid和上面回调函数中pthread_self获取到id不完全等价
+    pthread_create(&tid, NULL, th_func, (void *)&n);
+    printf("main thread id = %x\n", (unsigned int)pthread_self());
+    printf("main child thread ID: %x\n", (unsigned int)tid);
+    printf("main pid: %d\n", getpid());
+    sleep(1);
+    return 0;
+}
